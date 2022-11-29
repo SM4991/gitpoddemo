f9f0a41b1389d3136d8a708f51cdf1bdac976734
SM4991 <mahawarsurabhi@gmail.com>
2022-11-29 10:05:36 +0000

    java repository work

--- gitpod-react/src/components/main-container.js
+++ gitpod-react/src/components/main-container.js
@@ -12,7 +12,7 @@ export default class MainContainer extends Component {
           <div>
             <img src="https://dka575ofm4ao0.cloudfront.net/pages-transactional_logos/retina/237658/02205996e1e84567d4fe3458ef0c01ce895afcf6.png" width="480" height="200"></img>
           </div>
-          <img src="https://loremflickr.com/1000/600/mountains"/>
+          <img src="https://loremflickr.com/1000/600/cats"/>
         </div>
       </div>
     );
