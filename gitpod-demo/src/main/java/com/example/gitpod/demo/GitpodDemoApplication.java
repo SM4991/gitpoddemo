b47a45ee10367b5ff5d81d7d1252c1e8a422f1d8
PANKAJ172 <pjangid172@gmail.com>
2022-11-29 13:18:46 +0530

    re-create java project

---
+++ gitpod-demo/src/main/java/com/example/gitpod/demo/GitpodDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitpod.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitpodDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitpodDemoApplication.class, args);
+	}
+
+}
---
+++ gitpod-demo/src/test/java/com/example/gitpod/demo/GitpodDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitpod.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitpodDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
